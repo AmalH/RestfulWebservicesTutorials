b22fa70f7e558af5584b7768e4eb76d248873d26
AmalH <hichri.amal@hotmail.fr>
2018-01-31 02:20:34 +0100

    get/post/delete for booksResource

--- CrudsREST/src/net/amalhichri/restcruds/resources/BookResource.java
+++ CrudsREST/src/net/amalhichri/restcruds/resources/BookResource.java
@@ -1,5 +1,40 @@
 package net.amalhichri.restcruds.resources;
 
-public class BookResource {
+import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import net.amalhichri.restcruds.business.BooksBusiness;
+import net.amalhichri.restcruds.entities.Book;
+
+@Path("/books")
+public class BookResource {
+	
+	BooksBusiness booksBusiness = new BooksBusiness();
+
+	/** createBook **/
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response createRdv(Book book){
+		if (booksBusiness.addBook(book)) {
+			return Response.status(Status.CREATED).build();
+		} else {
+			return Response.status(Status.NOT_FOUND).build();
+		}
+	}
+	
+	/** getBook **/
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public List<Book> getBooks() {
+		return booksBusiness.getbooksList();
+	}
+	
 }
